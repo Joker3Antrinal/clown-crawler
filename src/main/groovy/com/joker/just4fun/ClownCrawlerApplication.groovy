9eae1369de252736b942f3807bfe79aa28252690
yaowang <18584564865@163.com>
2024-01-29 15:39:18 +0800

    Start afresh

---
+++ src/main/groovy/com/joker/just4fun/ClownCrawlerApplication.groovy
@@ -0,0 +1,12 @@
+package com.joker.just4fun
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ClownCrawlerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ClownCrawlerApplication, args)
+	}
+}
